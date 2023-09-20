fcb92af6ee57bd84eae5631b26034e194a07ef31
Gabriel Roux <65895832+gabriel-roux@users.noreply.github.com>
2023-09-20 09:23:45 -0300

    Developer (#7)
    * remove blank space
    
    * fix: animation landingPage
    
    * faq init
    
    * code review, and redo the faq list in stitches
    
    * code review, and redo the faq list in stitches
    
    * Resolved merge conflict by incorporating both suggestions.
    
    * Resolved merge conflict by incorporating both suggestions.
    
    * added global section, removed main grid
    
    * fix: link instagram
    
    * fixed disruptive section
    
    * implement autoplay for muted video elements
    
    ---------
    
    Co-authored-by: kwy404 <thekaway404@gmail.com>
    Co-authored-by: DataDavila <andre_x.pc@hotmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,32 @@ import { globalStyles } from '@/styles/global'
 import { WithApollo } from '@/libs/apollo'
 import 'react-toastify/dist/ReactToastify.css'
 import { appWithTranslation } from 'next-i18next'
+import { useEffect } from 'react'
 
 function App({ Component, pageProps }: AppProps) {
   globalStyles()
+
+  useEffect(() => {
+    const videos = document.querySelectorAll<HTMLVideoElement>(
+      'video[autoplay][muted]',
+    )
+
+    const playVideo = function (this: HTMLVideoElement) {
+      this.play().catch((error: unknown) => {
+        console.error('Autoplay error:', error)
+      })
+    }
+
+    videos.forEach((video) => {
+      video.addEventListener('canplay', playVideo)
+    })
+
+    return () => {
+      videos.forEach((video) => {
+        video.removeEventListener('canplay', playVideo)
+      })
+    }
+  }, [])
 
   return (
     <WithApollo>
